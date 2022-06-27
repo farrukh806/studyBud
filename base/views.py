27447e6953739d3bc1795a9df5d19101874fff9d
Farrukh <farrukhjamal806@gmail.com>
2022-06-27 13:48:45 +0530

    Create rooms feature added

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 import imp
-from django.shortcuts import render
+import re
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 # Create your views here.
 
 # rooms = []
@@ -14,3 +16,27 @@ def room(request, pk):
    
     context = {'room': room}
     return render(request, 'room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = { 'form' : form }
+    return render(request, 'room_form.html', context)
+
