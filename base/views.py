e957bbe52673e985c054930b04f262e7c19ea27b
Farrukh <farrukhjamal806@gmail.com>
2022-06-27 10:29:32 +0530

    Migrations added

---
+++ base/views.py
@@ -0,0 +1,16 @@
+import imp
+from django.shortcuts import render
+from .models import Room
+# Create your views here.
+
+# rooms = []
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+   
+    context = {'room': room}
+    return render(request, 'room.html', context)
