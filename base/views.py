fba45999e18fe973f123b8df65f8a92a6d337df9
Farrukh <farrukhjamal806@gmail.com>
2022-06-28 09:50:40 +0530

    Search feature added

--- base/views.py
+++ base/views.py
@@ -1,14 +1,20 @@
 import imp
 import re
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
 
 # rooms = []
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    query = request.GET.get('q')
+    
+    if query == None:
+        query = ''
+    rooms = Room.objects.filter(Q(topic__name__icontains=query) | Q(name__icontains=query)| Q(description__icontains=query))
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'home.html', context)
 
 def room(request, pk):
@@ -40,3 +46,10 @@ def updateRoom(request, pk):
     context = { 'form' : form }
     return render(request, 'room_form.html', context)
 
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    print('delete route')
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'delete.html', {'obj':room})
